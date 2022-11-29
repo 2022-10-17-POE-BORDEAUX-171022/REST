f8929ce6d09adce94009f9f71de4b70ac5e9ee64
AllanBernier <allan.bernier1@gmail.com>
2022-11-29 11:11:14 +0100

    JPA REST

--- example2/src/main/java/com/rest/controller/JsonController.java
+++ example2/src/main/java/com/rest/controller/JsonController.java
@@ -1,7 +1,6 @@
 package com.rest.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,114 +10,75 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
-import com.rest.entity.User;
+import com.rest.dao.UserDAO;
+import com.rest.modele.User;
 
 
 @WebServlet("/json")
 public class JsonController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	List<User> users = new ArrayList();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		List<User> users = UserDAO.getAllUser();
+		String usersJson = new Gson().toJson(users);
+		
 		response.setContentType("application/json");
-		response.setStatus(200);
-		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(usersJson);
 		
-		
-		response.getWriter().write( new Gson().toJson(users));
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email"); 
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
 		
-		try {
-			
-			int id =  Integer.parseInt( request.getParameter("id"));
-			int age =  Integer.parseInt( request.getParameter("age"));
-			
-			String name = request.getParameter("name");
-			String email = request.getParameter("email");
-			String password = request.getParameter("password");
-			
-			
-			if (name == null || email == null || password == null) {
-				throw new Exception();				
-			} else {
-				User u = new User(id, name, email, password, age);
-				users.add(u);
+		if ( email != null && login != null && password != null) {
+			if ( UserDAO.createUser(email, login, password))
 				response.setStatus(200);
-				response.getWriter().write("User created");
-			}
-			
-
-		} catch (Exception e) {
+			else
+				response.setStatus(424);
+		} else {
 			response.setStatus(400);
-
 		}
 	}
 	
 	
 	@Override
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		try {
-			
-			int id =  Integer.parseInt( request.getParameter("id"));
-			int age =  Integer.parseInt( request.getParameter("age"));
-			
-			String name = request.getParameter("name");
-			String email = request.getParameter("email");
+			String email = request.getParameter("email"); 
+			String login = request.getParameter("login");
 			String password = request.getParameter("password");
 			
-			if (name == null || email == null || password == null) {
-				throw new Exception();				
+			int id = Integer.parseInt( request.getParameter("id") );	
+			
+			if ( email != null && login != null && password != null) {
+				UserDAO.updateUser(id, email, login, password);
 			} else {
-				
-				User old = getUserById(id);
-				if (old != null) {
-					users.remove(old);
-					User newUser = new User(id, name, email, password, age);
-					users.add(newUser);
-					response.setStatus(200);
-				} else {
-					response.setStatus(404);
-				}
-			}	
-		} catch (Exception e) {
+				response.setStatus(400);
+			}
+		} catch (NumberFormatException e) {
 			response.setStatus(400);
 		}
+		
+
 	}
 
 	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
-			
-			int id =  Integer.parseInt( request.getParameter("id"));
-			
-			User u = getUserById(id);
-			
-			if (u != null) {
-				users.remove(u);
-				response.setStatus(200);
-			} else {
-				response.setStatus(404);
-			}
-		} catch (Exception e) {
+			int id = Integer.parseInt( request.getParameter("id") );
+			UserDAO.deleteUserById(id);
+			response.setStatus(200);				
+		} catch (NumberFormatException e) {
 			response.setStatus(400);
-
 		}
 	}
-	
-	
-	private User getUserById(int id) {
-		for (User u : users) { 
-			if (u.getId() == id) {
-				return u;
-			}
-		}
-		return null;
-	}
-	
-	
 }
+
+
+
